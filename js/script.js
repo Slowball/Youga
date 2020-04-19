74d9ed9dc5cc693e3483cc68b2ffc1ea294426ab
Serhii <monteki93@gmail.com>
2020-04-19 15:44:44 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -31,4 +31,50 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+    // add timer next
+
+    let deadLine = '2028-10-21';
+
+    function getTimeRemaining (endtime) {
+        let x = Date.parse(endtime) - Date.parse(new Date());
+        let seconds = Math.floor((x/1000) % 60);
+        let minutes = Math.floor((x/1000/60) % 60);
+        let hours = Math.floor(x/(1000*60*60));
+        return {
+            'total' : x,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours,
+        };
+    }
+
+    function setClock (id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock () {
+            let x = getTimeRemaining(endtime);
+
+            function addZero (num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(x.hours);
+            minutes.textContent = addZero(x.minutes);
+            seconds.textContent = addZero(x.seconds);
+
+            if (x.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadLine);
 });
