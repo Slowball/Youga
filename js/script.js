e0347934c9fdaa9c2b95daa83c65a2bedf6e5215
Serhii <monteki93@gmail.com>
2020-04-20 18:35:32 +0300

    add server

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTagContent (a) {
+    function hideTagContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
+
     hideTagContent(1);
 
-    function showTabContent (b) {
+    function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
+
     info.addEventListener('click', function (event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
@@ -35,30 +37,30 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let deadLine = '2028-10-21';
 
-    function getTimeRemaining (endtime) {
+    function getTimeRemaining(endtime) {
         let x = Date.parse(endtime) - Date.parse(new Date());
-        let seconds = Math.floor((x/1000) % 60);
-        let minutes = Math.floor((x/1000/60) % 60);
-        let hours = Math.floor(x/(1000*60*60));
+        let seconds = Math.floor((x / 1000) % 60);
+        let minutes = Math.floor((x / 1000 / 60) % 60);
+        let hours = Math.floor(x / (1000 * 60 * 60));
         return {
-            'total' : x,
-            'seconds' : seconds,
-            'minutes' : minutes,
-            'hours' : hours,
+            'total': x,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours,
         };
     }
 
-    function setClock (id, endtime) {
+    function setClock(id, endtime) {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
-        function updateClock () {
+        function updateClock() {
             let x = getTimeRemaining(endtime);
 
-            function addZero (num) {
+            function addZero(num) {
                 if (num <= 9) {
                     return '0' + num;
                 } else return num;
@@ -76,6 +78,7 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     }
+
     setClock('timer', deadLine);
 
     // modal
@@ -93,4 +96,94 @@ window.addEventListener('DOMContentLoaded', function() {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     })
+
+    //form
+
+    let mesage = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро свяжемся',
+        failure: "Error"
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = mesage.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = mesage.success;
+            } else {
+                statusMessage.innerHTML = mesage.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+    //form 2
+
+    let messages = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро свяжемся',
+        failure: "Error"
+    };
+
+    let forma = document.querySelector('.contact-form'),
+        inputs = forma.getElementsByTagName('input'),
+        statusMess = document.createElement('div');
+
+    statusMess.classList.add('status');
+    forma.addEventListener('submit', function (event) {
+        event.preventDefault();
+        forma.appendChild(statusMess);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        let formData = new FormData(forma);
+
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = mesage.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = mesage.success;
+            } else {
+                statusMessage.innerHTML = mesage.failure;
+            }
+        });
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+
+    });
 });
